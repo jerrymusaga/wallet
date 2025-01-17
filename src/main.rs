4af81b3269826f881d850bbd9b43fbb9a0e7e257
jerrymusaga <musagatechnology@gmail.com>
2025-01-17 09:00:28 +0100

    internal helper method to derive address from seed

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use alloy_primitives::Address;
 // use alloy_signer::{Signer};
-use k256::{ecdsa::SigningKey, PublicKey};
+use k256::{ecdsa::SigningKey, sha2::Sha512, PublicKey};
 use tiny_keccak::{Keccak, Hasher};
 use rand::rngs::OsRng;
-use anyhow::Result;
+use anyhow::{Error, Ok, Result};
+use hmac::{Hmac, Mac};
+
 
 #[derive(Debug)]
 pub struct Wallet {
@@ -14,7 +16,7 @@ pub struct Wallet {
 }
 
 impl Wallet {
-    fn new_wallet() -> Result<Self> {
+    pub fn new_wallet() -> Result<Self> {
         // generating random wallet
         let private_key = SigningKey::random(&mut OsRng);
         let public_key = private_key.verifying_key().to_encoded_point(false);
@@ -38,6 +40,42 @@ impl Wallet {
         })
     }
 
+
+    //internal helper function
+    fn derive_wallet_from_seed(seed: &[u8]) -> Result<Self> {
+        let mut hmac = Hmac::<Sha512>::new_from_slice(b"Bitcoin seed")
+                                                                            .map_err(|e| Error::msg(format!("HMAC Error : {e}")))?;
+                                                                            // or I can use the .map_err(|e| format!("Error: {}", e)).into()?;
+        hmac.update(seed);
+        let result = hmac.finalize().into_bytes();
+        
+        // Use first 32 bytes as private key
+        let private_key_bytes = &result[..32];
+        let private_key = SigningKey::from_bytes(private_key_bytes.into())
+                                                            .map_err(|e| Error::msg(format!("Invalid private key {e}")))?;
+        
+        let public_key = private_key.verifying_key().to_encoded_point(false);
+
+        // generate eth address
+        let mut hasher = Keccak::v256();
+        let mut hash = [0u8; 32];
+        hasher.update(&public_key.as_bytes()[1..]);
+        hasher.finalize(&mut hash);
+
+        let mut address_bytes = [0u8; 20];
+        address_bytes.copy_from_slice(&hash[12..32]);
+        let address = Address::from_slice(&address_bytes);
+
+        let public_key = (*private_key.verifying_key()).into();
+
+        Ok(Self {
+            private_key,
+            public_key,
+            address,
+            mnemonic: None
+        })
+    }
+
     pub fn get_address(&self) -> String {
         format!("{:#x}", self.address)
     }
@@ -46,7 +84,9 @@ impl Wallet {
 fn main() -> Result<()> {
     let wallet = Wallet::new_wallet()?;
 
-    println!("Wallet Address: {}", wallet.get_address());
+    println!("{:?}", wallet);
+
+    // println!("Wallet Address: {}", wallet.get_address());
 
     Ok(())
 
